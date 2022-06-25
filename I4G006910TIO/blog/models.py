16b6a8cc77d7b18344df99b871994faa543125d9
Saudat Sanni <105182062+Saudat83@users.noreply.github.com>
2022-06-25 14:10:46 -0700

    commit on main

---
+++ I4G006910TIO/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.db import get_user_model
+
+
+# Create your models here.
+from django.db import Post
+
+class Post(models.Model):
+
+# DB Fields
+    Title =  title = models.CharField(max_length=200)
+    Text = models.TextField("Any amount of text")
+    Author = get_user_model("A Foreign Key to the current user model")
+    Created_date = models.DateTimeField("A date-time column")
+    Published_date = models.DateTimeField("A date-time column")
