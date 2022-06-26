c2a56c66a626471ca250ab8236a09b96b2c1048a
Saudat Sanni <105182062+Saudat83@users.noreply.github.com>
2022-06-26 05:59:56 -0700

    commit on main

--- I4G006910TIO/blog/apps.py
+++ I4G006910TIO/blog/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model 
+from django.db import Post 
+from django.db import models
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+
+class Post(models.Model):
+
+# DB Fields
+    Title =  title = models.CharField(max_length=200)
+    Text = models.TextField("Any amount of text")
+    Author = get_user_model("A Foreign Key to the current user model")
+    Created_date = models.DateTimeField("A date-time column")
+    Published_date = models.DateTimeField("A date-time column")
--- I4G006910TIO/blog/models.py
+++ I4G006910TIO/blog/models.py
@@ -1,13 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model 
-from django.db import Post
+
 
 # Create your models here.
-class Post(models.Model):
-
-# DB Fields
-    Title =  title = models.CharField(max_length=200)
-    Text = models.TextField("Any amount of text")
-    Author = get_user_model("A Foreign Key to the current user model")
-    Created_date = models.DateTimeField("A date-time column")
-    Published_date = models.DateTimeField("A date-time column")
