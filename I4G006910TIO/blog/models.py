a119d6328d4c5ae4b6963bb755912aedf8aa215b
Saudat Sanni <105182062+Saudat83@users.noreply.github.com>
2022-06-26 05:45:41 -0700

    commit on main

--- I4G006910TIO/blog/models.py
+++ I4G006910TIO/blog/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model 
-
-
-# Create your models here.
 from django.db import Post
 
+# Create your models here.
 class Post(models.Model):
 
 # DB Fields
